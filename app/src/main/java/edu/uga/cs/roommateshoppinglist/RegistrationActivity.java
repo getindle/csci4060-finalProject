7080126a1487f9072069c2acabb2af2fd96375f8
George Tindle <gtindle@Georges-MacBook-Pro.local>
2024-11-19 15:11:15 -0500

    login screen is first screen to appear

--- app/src/main/java/edu/uga/cs/roommateshoppinglist/RegistrationActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshoppinglist/RegistrationActivity.java
@@ -11,6 +11,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button loginB;
@@ -52,6 +54,10 @@ public class RegistrationActivity extends AppCompatActivity {
 
             // ** INSERT ON CLICK REGISTRATION IMPLEMENTATION HERE **
 
+
+            Intent intent = new Intent(view.getContext(), ListActivity.class);
+            startActivity(intent);
+            finish();
         }
     }
 }
